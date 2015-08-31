57ea6ac9eb9cda650311b25cb9f59bace4456a72
jiimbo <jonathan.masci@gmail.com>
2015-08-31 14:20:32 +0200

    Checks to avoid re-computing output

--- extract_geovec.m
+++ extract_geovec.m
@@ -2,6 +2,10 @@ function extract_geovec(srcpath, dstpath, geovec_params)
 
 fnames = dir(fullfile(srcpath, '*.mat'));
 parfor i = 1 : length(fnames)
+    if exist(fullfile(dstpath, fnames(i).name), 'file')
+        fprintf('%s already processed, skipping\n', fnames(i).name)
+        continue
+    end
     fprintf('Processing %s\n', fnames(i).name)
     tmp = load(fullfile(srcpath, fnames(i).name));
     [desc, ~] = calc_geovec(tmp.Phi, tmp.Lambda, geovec_params);
